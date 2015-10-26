b006285c47eb8cbb244ebb9480bd0cf9689cdcca
unknown <paynebri004@779CTE220LAB03.alpine.local>
2015-10-26 10:01:20 -0600

    started new UI

---
+++ Random Number Generator/myForm.cpp
@@ -0,0 +1,15 @@
+#include "myForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	RandomNumberGenerator::myForm form;
+	Application::Run(%form);
+}
